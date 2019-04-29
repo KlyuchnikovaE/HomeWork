6fed24ab1625734a7298ab0d689ed3df2d3bd482
Евгения <klyuchnukova_enemy@list.ru>
2019-04-29 09:45:16 +0400

    Исключения

---
+++ MyException/MyException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = x / 2;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Исключение:{ex.Message}");
+                Console.WriteLine($"Метод:{ex.TargetSite}");
+                Console.WriteLine($"Трассировка стека:{ex.StackTrace}");
+            }
+            Console.Read();
+
+        }
+    }
+}
